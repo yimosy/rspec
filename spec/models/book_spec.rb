94ddd7917c5764d902c07c4690df50a6ba3615c1
yimosy <yimosy@gmail.com>
2017-01-17 15:32:47 +0900

    model test

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # 2と1を足すと3になること
+  it "adds 2 and 1 to make 3" do
+    expect(2 + 1).to eq 3
+  end
+  # 3と1を足すと4になること
+  it "adds 3 and 1 to make 3" do
+  end
+
+#  pending "add some examples to (or delete) #{__FILE__}"
 end
